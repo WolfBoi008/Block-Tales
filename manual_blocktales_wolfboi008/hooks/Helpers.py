c24c63208fdcf13c81c4da01ac10385676c7b7c0
WolfBoi008 <109931785+WolfBoi008@users.noreply.github.com>
2026-03-21 02:04:57 +0000

    [create-pull-request] automated change

--- manual_blocktales_wolfboi008/hooks/Helpers.py
+++ manual_blocktales_wolfboi008/hooks/Helpers.py
@@ -19,3 +19,7 @@ def before_is_item_enabled(multiworld: MultiWorld, player: int, item: "ManualIte
 # Return True to enable the location, False to disable it, or None to use the default behavior
 def before_is_location_enabled(multiworld: MultiWorld, player: int, location: "ManualLocation") -> Optional[bool]:
     return None
+
+
+def before_is_event_enabled(multiworld: MultiWorld, player: int, event: dict[str, Any]) -> Optional[bool]:
+    return None
